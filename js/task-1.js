5f080481d8c722284d0ea86cacceee83947887b4
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-18 00:00:48 +0200

    hm done

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const listItems = document.querySelectorAll('.item');
-console.log('Number of categories: ', listItems.length);
-
-listItems.forEach(listItem => {
-  console.log('Category: ', listItem.querySelector('h2').textContent);
-  console.log('Elements: ', listItem.querySelectorAll('li').length);
-});
